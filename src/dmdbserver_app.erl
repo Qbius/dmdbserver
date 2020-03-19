cd0cd39b198780d24e250539fd49d31a934d103c
Qbius <mich@lsurowka.email>
2020-03-20 00:35:21 +0100

    added cowboy router

--- src/dmdbserver_app.erl
+++ src/dmdbserver_app.erl
@@ -10,6 +10,15 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+	    {'_', [
+		    {"/websocket", ws, []}
+	    ]}
+	]),
+	{ok, _} = cowboy:start_clear(my_http_listener,
+	    [{port, 8080}],
+	    #{env => #{dispatch => Dispatch}}
+	),
     dmdbserver_sup:start_link().
 
 stop(_State) ->
