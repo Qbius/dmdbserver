83a8b0c179c13479fb849f38bcd6ea1ecfe9d610
Qbius <mich@lsurowka.email>
2020-03-20 17:49:52 +0100

    wrong app name

--- src/dmdbserver_app.erl
+++ src/dmdbserver_app.erl
@@ -13,9 +13,9 @@ start(_StartType, _StartArgs) ->
     Dispatch = cowboy_router:compile([
 	    {'_', [
 			{"/", cowboy_static, {priv_file, dmdbserver, "index.html"}},
-			{"/[...]", cowboy_static, {priv_dir, life, "."}},
-			{"/dm_images/[...]", cowboy_static, {priv_dir, life, "dm_images"}},
-			{"/icons/[...]", cowboy_static, {priv_dir, life, "icons"}},
+			{"/[...]", cowboy_static, {priv_dir, dmdbserver, "."}},
+			{"/dm_images/[...]", cowboy_static, {priv_dir, dmdbserver, "dm_images"}},
+			{"/icons/[...]", cowboy_static, {priv_dir, dmdbserver, "icons"}},
 		    {"/websocket", ws, []}
 	    ]}
 	]),
